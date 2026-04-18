d67cf1881d0d9a763994b54513d122df7ec5f1c8
DaviQueirozLima <daviqueirozlima6@gmail.com>
2026-04-18 12:18:11 -0300

    feat: :sparkles: add Stock response, request and validator

---
+++ src/StockAlert.Application/Stock/Validators/RegisterStockValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using StockAlert.Communication.Requests.Stock;
+
+namespace StockAlert.Application.Stock.Validators
+{
+    public class RegisterStockValidator : AbstractValidator<RegisterStockRequest>
+    {
+        public RegisterStockValidator() 
+        { 
+            RuleFor(x => x.Symbol)
+                .NotEmpty().WithMessage("The stock symbol is required.")
+                .MinimumLength(3).WithMessage("The stock symbol must be at least 3 characters long.")
+                .MaximumLength(10).WithMessage("The stock symbol must be at most 10 characters long.")
+                .Must(symbol => !string.IsNullOrWhiteSpace(symbol)).WithMessage("Stock symbol cannot be empty.")
+                .Matches(@"^[a-zA-Z0-9]+$").WithMessage("The symbol must contain only letters and numbers.");
+
+        }
+    }
+}
