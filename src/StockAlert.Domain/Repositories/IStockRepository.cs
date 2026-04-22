4cf1630085a7d217184a50e60d5e4b9f8503ef2a
DaviQueirozLima <daviqueirozlima6@gmail.com>
2026-04-22 18:26:01 -0300

    feat: :sparkles: add workflow support to Stock and AlertRule

--- src/StockAlert.Domain/Repositories/IStockRepository.cs
+++ src/StockAlert.Domain/Repositories/IStockRepository.cs
@@ -5,6 +5,7 @@ namespace StockAlert.Domain.Repositories
     public interface IStockRepository
     {
         Task<Stock?> GetBySymbolAndUserIdAsync(string symbol, Guid userId);
+        Task<IEnumerable<Stock>> GetAllAsync();
         Task AddAsync(Stock stock);
         Task UpdateAsync(Stock stock);
     }
